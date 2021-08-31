1be6a8c58ebf2c9f0360daa573977361af0ab2f4
Lucas Stellet <lustepe@gmail.com>
2021-08-31 11:58:14 -0300

    chore: add argon2 library

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Blogex.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:argon2_elixir, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
