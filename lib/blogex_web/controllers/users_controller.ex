31e6442d2115dab6db78a40e49ead8a37c333ec8
Lucas Stellet <lustepe@gmail.com>
2021-09-01 20:25:17 -0300

    feat: change logic to delete user that has a valid token

--- lib/blogex_web/controllers/users_controller.ex
+++ lib/blogex_web/controllers/users_controller.ex
@@ -1,17 +1,21 @@
 defmodule BlogexWeb.UsersController do
   use BlogexWeb, :controller
 
+  alias BlogexWeb.Auth.Guardian
   alias BlogexWeb.FallbackController
 
   action_fallback FallbackController
 
+  import Guardian.Plug, only: [current_resource: 1]
+
   alias Blogex.User
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Blogex.create_user(params) do
+    with {:ok, %User{} = user} <- Blogex.create_user(params),
+         {:ok, token, _claim} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> render("create.json", user: user)
+      |> render("create.json", token: token)
     end
   end
 
@@ -31,8 +35,9 @@ defmodule BlogexWeb.UsersController do
     |> render("index.json", users: users)
   end
 
-  def delete(conn, %{"id" => id}) do
-    with {:ok, %User{} = _user} <- Blogex.delete_user(id) do
+  def delete(conn, _params) do
+    with {:ok, %{id: id}} <- current_resource(conn),
+         {:ok, %User{} = _user} <- Blogex.delete_user(id) do
       conn
       |> put_status(:no_content)
       |> text("")
