8ab0738558c1d165a2eca1f1eb16136d74779f1d
Lucas Stellet <lustepe@gmail.com>
2021-09-01 11:16:50 -0300

    chore: create fallback controller

---
+++ lib/blogex_web/controllers/fallback_controller.ex
@@ -0,0 +1,16 @@
+defmodule BlogexWeb.FallbackController do
+  @moduledoc """
+  Handle errors in the BlogexWeb application
+  """
+
+  use BlogexWeb, :controller
+
+  alias BlogexWeb.ErrorView
+
+  def call(conn, {:error, %{status: status, result: result}}) do
+    conn
+    |> put_status(status)
+    |> put_view(ErrorView)
+    |> render("error.json", result: result)
+  end
+end
