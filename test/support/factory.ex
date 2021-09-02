cb462f890aefe97d0634b94647954171f76bd5ba
Lucas Stellet <lustepe@gmail.com>
2021-09-02 19:45:57 -0300

    test: add post factory to help with tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Blogex.Factory do
   @moduledoc false
   use ExMachina.Ecto, repo: Blogex.Repo
 
-  alias Blogex.User
+  alias Blogex.{Post, User}
 
   def user_params_factory do
     %{
@@ -20,6 +20,15 @@ defmodule Blogex.Factory do
       image: "https://link.com",
       password: "123senha",
       id: "c4d98b79-2228-41b7-8c75-6473a586d54f"
+    }
+  end
+
+  def post_factory do
+    %Post{
+      content: "Post content",
+      title: "Post title",
+      id: "aa355add-9134-40ab-a09f-8aa8c21de001",
+      user_id: "c4d98b79-2228-41b7-8c75-6473a586d54f"
     }
   end
 end
