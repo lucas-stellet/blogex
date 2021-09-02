03a2c40b8f60f49dff399b6066df2d01881f6f4d
Lucas Stellet <lustepe@gmail.com>
2021-09-02 13:32:18 -0300

    chore: add post facade funtions

--- lib/blogex.ex
+++ lib/blogex.ex
@@ -1,9 +1,13 @@
 defmodule Blogex do
   @moduledoc false
 
+  alias Blogex.Posts.Create, as: PostCreate
+  alias Blogex.Posts.Get, as: PostGet
+  alias Blogex.Posts.Update, as: PostUpdate
+  alias Blogex.Posts.Delete, as: PostDelete
   alias Blogex.Users.Create, as: UserCreate
-  alias Blogex.Users.Get, as: UserGet
   alias Blogex.Users.Delete, as: UserDelete
+  alias Blogex.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
@@ -12,4 +16,16 @@ defmodule Blogex do
   defdelegate get_all_users(), to: UserGet, as: :all
 
   defdelegate delete_user(id), to: UserDelete, as: :call
+
+  defdelegate create_post(params), to: PostCreate, as: :call
+
+  defdelegate get_all_posts(), to: PostGet, as: :all
+
+  defdelegate get_post_by_id(params), to: PostGet, as: :by_id
+
+  defdelegate update_post(params), to: PostUpdate, as: :call
+
+  defdelegate delete_post(params), to: PostDelete, as: :call
+
+  defdelegate search_post(params), to: PostGet, as: :by_term
 end
