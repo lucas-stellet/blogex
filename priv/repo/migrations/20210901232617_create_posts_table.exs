8c04b252d6d0aecbfbb15f1bf99d3cea3f3a4b4c
Lucas Stellet <lustepe@gmail.com>
2021-09-02 17:43:43 -0300

    fix: delete posts when user is deleted

--- priv/repo/migrations/20210901232617_create_posts_table.exs
+++ priv/repo/migrations/20210901232617_create_posts_table.exs
@@ -6,7 +6,7 @@ defmodule Blogex.Repo.Migrations.CreatePostsTable do
       add :id, :uuid, primary_key: true
       add :title, :string
       add :content, :string
-      add :user_id, references(:users, on_delete: :nothing, type: :uuid)
+      add :user_id, references(:users, on_delete: :delete_all, type: :uuid)
 
       timestamps()
     end
